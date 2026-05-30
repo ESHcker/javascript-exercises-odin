dde9efa6ee7be1c9235e3044ca01b91867ae71b4
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-05-30 12:46:09 +0100

    Add my sol for exercise 7 js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = "";
+    
+    for(index = string.length; index >= 0; index--){
+        result += string.charAt(index);
+    }
 
+    return result;
 };
 
 // Do not edit below this line

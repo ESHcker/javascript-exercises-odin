ab13163bed972f14f163f020d3183ddc0b8633ad
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-05-30 12:34:03 +0100

    Add my sol for exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeatString) {
+    let result = "";
+    if(numberOfRepeatString < 0) return "ERROR";
+    while(numberOfRepeatString > 0){
+        result+=string;
+        numberOfRepeatString--;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line

cb012cd3dcb99df17adaab2b0bdbe23e1b420f17
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-05-30 12:47:12 +0100

    Add my solutions for exercises 8-11 js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToDel) {
+    for (index = 1; index < arguments.length; index++) {
+        elementToDel = arguments[index];
+        console.log(elementToDel);
+        let hasElementToDel = array.findIndex(element => element === elementToDel);
+        while(hasElementToDel != -1){
+            array.splice(hasElementToDel, 1);
+            hasElementToDel = array.findIndex(element => element === elementToDel);
+        }
+    }  
+    return array;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if(number1 < 0 || number2 < 0) return "ERROR";
+    if(number1 % 1 != 0 || number2 % 1 != 0) return "ERROR";
+    if(typeof number1 == "string" || typeof number2 == "string") return "ERROR";
 
+    let lowestNumber = number1 > number2 ? number2 : number1;
+    let greaterNumber = number1 > number2 ? number1 : number2;
+    let result = 0;
+    while(lowestNumber <= greaterNumber ){
+        result+=lowestNumber;
+        lowestNumber++;
+    }
+    return result;
 };
 
 // Do not edit below this line

4517fd5c9a39743768566cfd621a6736491af97d
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-06-09 18:51:27 +0100

    Add my sol for exercise 14 js Modify package json

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = Number(number);
+    if(number < 0) return "OOPS";
+    switch(number){
+        case 0: return 0;
+        case 1: return 1;
+        default:
+            return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 };
 
 // Do not edit below this line

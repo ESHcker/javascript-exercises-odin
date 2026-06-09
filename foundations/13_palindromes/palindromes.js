babe82072bafed44f3b1e0b85914726c922075da
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-06-09 18:34:51 +0100

    Add my sol for exercise 13 js

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
+const palindromes = function (phrase) {
+    const cleanString = phrase
+     .toLowerCase()
+     .replace(/[\s_¡¿!.',/:;?()-]/g , "");
+    
+    const phraseReversed = cleanString.split("").reverse().join("");
+    return cleanString === phraseReversed;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

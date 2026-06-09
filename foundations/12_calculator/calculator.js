d7ff572e33fb93e9d44e744827d22db12b2a1860
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-06-09 10:32:54 +0100

    Add sol for exercise 12 js

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayNumbers) {
+	return arrayNumbers.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayNumbers) {
+return arrayNumbers.reduce((sum, number) => sum * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 1 || number === 0) return 1;
+  if(number >= 2){
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line

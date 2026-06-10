76041bb63c48ef746c1df632c4ea3e9f480a2dc1
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-06-10 10:28:20 +0100

    Add my sol for exercise 16 js

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = [{name: "", age: "0"}];
+    for(let index = 0; index < people.length; index++){
+        let ageOfPerson = getAgeOfPerson(
+            people[index].yearOfBirth,
+            people[index].yearOfDeath
+        );
+        if (ageOfPerson > oldestPerson[0].age){
+            oldestPerson[0]= { name: people[index].name,
+                age: ageOfPerson,
+            };
+              
+        }
+    }
+    return oldestPerson[0];
+}
 
-};
+function getAgeOfPerson(yearOfBirth, yearOfDeath){
+    if(typeof yearOfDeath === "undefined")yearOfDeath = new Date().getFullYear();
+    return yearOfDeath - yearOfBirth;
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
